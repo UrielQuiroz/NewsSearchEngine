c2001a1e166529286b5138d79e744643ea338adf
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-14 11:50:01 -0600

    Custom Hook
    Se creo un custom hook para crear el select

--- news-search-engine/src/components/Formulario.jsx
+++ news-search-engine/src/components/Formulario.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from '../css/Formulario.module.css'
+import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+
+    //Utilizar costom hook para el select
+    const [ categoria, SelectNoticias ] = useSelect();
+
     return (
         <div className={`${styles.buscador} row`}>
             <div className='col s12 m8 offset-m2'>
                 <form action="">
                     <h2 className={styles.heading}>Encuentra Noticias por Categoria</h2>
+
+                    <SelectNoticias/>
 
                     <div className='input-field col s12'>
                         <input 
